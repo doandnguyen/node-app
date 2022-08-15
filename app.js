1b778d9ee831c6eb3fa662ebd83c0325681999bd
doandnguyen <doan.nguyen@innomizetech.com>
2022-08-15 14:44:58 +0700

    Initial dev branch

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD! This is new update!'))
+app.get('/', (req, res) => res.send('Hello Argo CD! This is new update for dev'))
 app.listen(3000, () => console.log('Server ready'))
