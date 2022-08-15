30d279fe1fe7a422a8272e911a478a5fa5b74a94
doandnguyen <doan.nguyen@innomizetech.com>
2022-08-15 17:33:05 +0700

    revert

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello Argo CD! This is new update for dev'))
-app.listen(80, () => console.log('Server ready'))
+app.listen(3000, () => console.log('Server ready'))
